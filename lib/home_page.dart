41f3def32a033ffa012627285ac7629e78608815
Luis Santamaria <santamariaaguayoluisangel@gmail.com>
2025-02-13 13:32:45 -0600

    Main text added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -18,7 +18,42 @@ class _HomePageState extends State<HomePage> {
           Icons.menu,
           color: Colors.grey[800],
         ),
-        actions: [Icon(Icons.person)],
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(Icons.person),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          // 1. Texto principal (Text)
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Row(
+              children: [
+                Text("I want to eat "),
+                Text(
+                  "Eat",
+                  style: TextStyle(
+                    // Tamaño de letra
+                    fontSize: 32,
+                    // Negritas
+                    fontWeight: FontWeight.bold,
+                    // Subrayado
+                    decoration: TextDecoration.underline,
+                  ),
+                )
+              ],
+            ),
+          )
+
+          // 2. Pestañas (TabBar)
+
+          // 3. Contenido de pestañas (TabBarView)
+
+          // 4. Carrito (Car)
+        ],
       ),
     );
   }
